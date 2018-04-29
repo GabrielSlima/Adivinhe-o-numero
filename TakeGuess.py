055c9b309c203b24c9adca30368847849c2e1f7d
Gabriel Lima <34993129+HiroTatsuo@users.noreply.github.com>
2018-04-29 19:54:30 +0000

    Update TakeGuess.py

--- TakeGuess.py
+++ TakeGuess.py
@@ -3,9 +3,21 @@ numeroSecreto =  random.randint(1,20)
 print('O numero que estou pensando esta entre 1 e 20')
 
 #Dando ao usuario 6 oportunidades
+
 for tentativa in range (1,7):
 	print('Tente adivinhar:')
-	numero = int(input())
+	entrada = input()
+		
+	if entrada == '':
+		while True:
+			print('Tente adivinhar')
+			entrada = input()
+			if entrada != "":
+				break
+	try:
+		numero = int(entrada)
+	except ValueError:
+		print("Talvez o valor inserido não seja inteiro...")
 	if numero < numeroSecreto:
 		print('Seu numero está abaixo...')
 	elif numero > numeroSecreto: 
